bfa482e313f6776a4fbb59c3a64db201c1ab671d
prassepaul <paul.prasse@uni-potsdam.de>
2024-10-07 10:26:15 +0200

    fix gazebasevr idt

--- evaluate_classification_event_detection.py
+++ evaluate_classification_event_detection.py
@@ -8,8 +8,6 @@ import utils.helpers as helpers
 def get_argument_parser() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', '-d', type=str, default='sbsat')
-    parser.add_argument(
-        '--sampling-rate', type=int, default=1000)    
     parser.add_argument('--detection-method', type=str, default='ivt')
     args = parser.parse_args()
     return args
@@ -18,16 +16,17 @@ def get_argument_parser() -> argparse.Namespace:
 def evaluate(args):
     dataset = args.dataset
     detection_method = args.detection_method
-    sampling_rate = args.sampling_rate
     
-    detection_params = helpers.get_detection_params(detection_method)
+    if dataset == 'gazebasevr':
+        detection_params = helpers.get_detection_params(detection_method, sampling_rate=250)
+    else:
+        detection_params = helpers.get_detection_params(detection_method)
     label_columns = helpers.get_datset_labels(dataset)
     
     for label_column in label_columns:
         for detection_param in detection_params:
             exec_string = 'python train_classification_model.py --dataset ' + str(dataset) +\
                             ' --label-column ' + str(label_column) +\
-                            ' --sampling-rate ' + str(sampling_rate) +\
                             ' --detection-method ' + str(detection_method) +\
                             ' ' + detection_param
             os.system(exec_string)
